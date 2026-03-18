958cb63d82525532dd995d411ed17532353153b1
iaretechnician <iaretechnician@gmail.com>
2026-03-18 13:37:55 -0400

    Init

---
+++ Assets/Do not shoot Aliens/Game assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	
+	public float force;
+	public float lifetime;
+	
+	[HideInInspector]
+	public PlayerController player;
+	
+	void OnEnable(){
+		StartCoroutine(DestroySelf());
+	}
+	
+	void Update(){
+		transform.Translate(Vector3.forward * Time.deltaTime * force);
+	}
+	
+	void OnTriggerEnter(Collider other){
+		// Support hitting colliders on the enemy or any of its children
+		Enemy enemy = other.GetComponent<Enemy>();
+		if(enemy == null)
+			enemy = other.GetComponentInParent<Enemy>();
+		
+		if(enemy == null)
+			return;
+		
+		enemy.Hit();
+		player.DisableBullet(gameObject);
+	}
+	
+	IEnumerator DestroySelf(){
+		yield return new WaitForSeconds(lifetime);
+		
+		player.DisableBullet(gameObject);
+	}
+}
